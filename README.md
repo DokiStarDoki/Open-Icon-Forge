2dda2aa015f2425e8099f1c00ffb47b76e0f177b
KenWheadon <ken@smartfirm.com>
2025-04-30 00:41:55 -0300

    Add initial folder structure, icons, and data

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Open-Icon-Forge
-Open source library of icons created by image generation. 
+# open-icon-forge
---
+++ run-org.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Create Project Structure - Batch Runner
+REM Assumes Python is installed and added to PATH
+
+echo Running project setup...
+
+python scripts/organize-icons.py
+
+pause
